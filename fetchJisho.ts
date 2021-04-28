e8bd3e8bd6cd36c222300c8c95e7dfdc732bc441
kirinokirino <kirinokirino2501@gmail.com>
2021-04-28 13:34:19 +0300

    Adds interfaces to pass typescript checks.

--- fetchJisho.ts
+++ fetchJisho.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env -S deno run --no-check --allow-net=jisho.org
-import { printJson } from "./print_json.ts";
 const url = "https://jisho.org/api/v1/search/words";
 
 export async function fetchFromJisho(
@@ -10,7 +9,7 @@ export async function fetchFromJisho(
   if (page) {
     pageString = "&page=" + page;
   }
-  let query = `${url}?keyword=%23word %23common %3F*${keyword}*${pageString}`;
+  const query = `${url}?keyword=%23word %23common %3F*${keyword}*${pageString}`;
   console.info(query);
   return await fetch(query);
 }
@@ -26,26 +25,40 @@ export function getJson(
 }
 
 export async function getRelevantInfo(query: string) {
-  let json = await getJson(fetchFromJisho(query));
-  let result = [];
+  const json = await getJson(fetchFromJisho(query)) as JishoJSON;
+  const result: Entry[] = [];
 
   for (let i = 0; i < 50; i++) {
     if (json["data"][i] === undefined) break;
-    let entry = {};
-    entry.jlpt = json["data"][i]["jlpt"];
-    entry.tags = json["data"][i]["tags"];
-    entry.japanese = json["data"][i]["japanese"];
-    entry.senses = [];
-    if (json["data"][i]["senses"][0] !== undefined) {
-      entry.senses.push(json["data"][i]["senses"][0]["english_definitions"]);
-    }
-    if (json["data"][i]["senses"][1] !== undefined) {
-      entry.senses.push(json["data"][i]["senses"][1]["english_definitions"]);
-    }
-    if (json["data"][i]["senses"][2] !== undefined) {
-      entry.senses.push(json["data"][i]["senses"][2]["english_definitions"]);
-    }
+    const jlpt = json["data"][i]["jlpt"];
+    const tags = json["data"][i]["tags"];
+    const japanese = json["data"][i]["japanese"];
+    const senses: Sense[] = json["data"][i]["senses"].map((sense: Sense) => {
+      return sense["english_definitions"];
+    });
+    const entry: Entry = {
+      jlpt,
+      tags,
+      japanese,
+      senses,
+    };
     result.push(entry);
   }
   return result;
+}
+
+interface JishoJSON {
+  data: Array<Entry>;
+}
+
+export interface Entry {
+  jlpt: Record<string, unknown>;
+  tags: Record<string, unknown>;
+  japanese: Record<string, unknown>;
+  senses: Sense[];
+}
+
+interface Sense {
+  // deno-lint-ignore camelcase
+  english_definitions: Sense;
 }
