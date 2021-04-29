4ab39f7a24c9cea4903535712fdf6bbd139638cb
kirinokirino <kirinokirino2501@gmail.com>
2021-04-29 09:54:18 +0300

    TODO: filter copies of the same japanese-sense pairs

--- deps.ts
+++ deps.ts
@@ -4,3 +4,4 @@ export * as csv from "https://deno.land/std@0.95.0/encoding/csv.ts";
 export * as path from "https://deno.land/std@0.95.0/path/mod.ts";
 export { readAll } from "https://deno.land/std@0.95.0/io/mod.ts";
 export { delay } from "https://deno.land/std@0.95.0/async/mod.ts";
+export { walk, walkSync } from "https://deno.land/std@0.95.0/fs/mod.ts";
